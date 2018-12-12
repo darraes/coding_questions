463e0144765777c7ad645efc4aace2a69e186ea9
Daniel Arraes <daniel.arraes@gmail.com>
2018-12-11 21:23:31 -0800

    general push

---
+++ 2018/_data_structures_and_algorithms/diff.py
@@ -0,0 +1,49 @@
+def print_lines(X):
+    for x in X:
+        print(x)
+
+
+def lcs_lookup(X, Y):
+    m = len(X)
+    n = len(Y)
+    lookup = [[0 for y in range(len(Y) + 1)] for x in range(len(X) + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                lookup[i][j] = lookup[i - 1][j - 1] + 1
+            else:
+                lookup[i][j] = max(lookup[i][j - 1], lookup[i - 1][j])
+
+    return lookup
+
+
+def diff(X, Y, m, n, lookup, res):
+    if m > 0 and n > 0 and X[m - 1] == Y[n - 1]:
+        diff(X, Y, m - 1, n - 1, lookup, res)
+        res.append(X[m - 1])
+    elif n > 0 and (m == 0 or lookup[m][n - 1] >= lookup[m - 1][n]):
+        diff(X, Y, m, n - 1, lookup, res)
+        res.append("+" + Y[n - 1])
+    elif m > 0 and (n == 0 or lookup[m - 1][n] > lookup[m][n - 1]):
+        diff(X, Y, m - 1, n, lookup, res)
+        res.append("-" + X[m - 1])
+
+
+###############################################################
+
+
+import unittest
+
+
+class TestFunctions(unittest.TestCase):
+    def test_diff(self):
+        X = "ABCDFGHJQZ"
+        Y = "ABCDEFGIJKRXYZ"
+        res = []
+        diff(X, Y, len(X), len(Y), lcs_lookup(X, Y), res)
+        self.assertEqual("A B C D +E F G -H +I J -Q +K +R +X +Y Z", " ".join(res))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- 2018/_data_structures_and_algorithms/lcs_stuff.py
+++ 2018/_data_structures_and_algorithms/lcs_stuff.py
@@ -3,6 +3,11 @@ def print_lines(X):
         print(x)
 
 
+#############################################
+######### Longest Common Subsequence ########
+#############################################
+
+
 def lcs_length(X, Y):
     m = len(X)
     n = len(Y)
@@ -16,6 +21,23 @@ def lcs_length(X, Y):
                 lookup[i][j] = max(lookup[i][j - 1], lookup[i - 1][j])
 
     return lookup[m][n], lookup
+
+
+def lcs_length_rec(X, Y, m, n, cache):
+    if m == len(X) or n == len(Y):
+        return 0
+
+    if (m, n) in cache:
+        return cache[(m, n)]
+
+    if X[m] == Y[n]:
+        cache[(m, n)] = 1 + lcs_length_rec(X, Y, m + 1, n + 1, cache)
+    else:
+        cache[(m, n)] = max(
+            lcs_length_rec(X, Y, m + 1, n, cache), lcs_length_rec(X, Y, m, n + 1, cache)
+        )
+
+    return cache[(m, n)]
 
 
 def lcs(X, Y, m, n, lookup):
@@ -41,6 +63,88 @@ def lcs(X, Y, m, n, lookup):
         return ans
 
 
+def lcs_rec(X, Y, m, n, cache):
+    if m == len(X) or n == len(Y):
+        return []
+
+    if (m, n) in cache:
+        return cache[(m, n)]
+
+    if X[m] == Y[n]:
+        cache[(m, n)] = [X[m]] + lcs_rec(X, Y, m + 1, n + 1, cache)
+    else:
+        left = lcs_rec(X, Y, m + 1, n, cache)
+        right = lcs_rec(X, Y, m, n + 1, cache)
+        cache[(m, n)] = left if len(left) > len(right) else right
+
+    return cache[(m, n)]
+
+
+#############################################
+######### Longest Common Substring ##########
+#############################################
+
+
+def lcsubstring_length(X, Y):
+    m = len(X)
+    n = len(Y)
+    lookup = [[0 for y in range(len(Y) + 1)] for x in range(len(X) + 1)]
+
+    max_len = 0
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                lookup[i][j] = lookup[i - 1][j - 1] + 1
+                max_len = max(max_len, lookup[i][j])
+            else:
+                lookup[i][j] = 0
+
+    return max_len
+
+
+def lcsubstring(X, Y):
+    m = len(X)
+    n = len(Y)
+    lookup = [[0 for y in range(len(Y) + 1)] for x in range(len(X) + 1)]
+
+    max_len = end_m = end_n = 0
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                lookup[i][j] = lookup[i - 1][j - 1] + 1
+                if lookup[i][j] > max_len:
+                    max_len = lookup[i][j]
+                    end_m, end_n = i, j
+            else:
+                lookup[i][j] = 0
+
+    return X[end_m - max_len : end_m]
+
+
+#############################################
+######### Longest Palindromic Substring #####
+#############################################
+
+
+def lps_length(X, i, j, cache):
+    if i > j:
+        return 0
+    if i == j:
+        return 1
+
+    if (i, j) in cache:
+        return cache[(i, j)]
+
+    if X[i] == X[j]:
+        cache[(i, j)] = 2 + lps_length(X, i + 1, j - 1, cache)
+    else:
+        cache[(i, j)] = max(
+            lps_length(X, i + 1, j, cache), lps_length(X, i, j - 1, cache)
+        )
+
+    return cache[(i, j)]
+
+
 ###############################################################
 
 
@@ -49,7 +153,8 @@ import unittest
 
 class TestFunctions(unittest.TestCase):
     def test_lcs_length(self):
-        # self.assertEqual(4, lcs_length(list("ABCBDAB"), list("BDCABA"))[0])
+        self.assertEqual(4, lcs_length(list("ABCBDAB"), list("BDCABA"))[0])
+        self.assertEqual(4, lcs_length_rec(list("ABCBDAB"), list("BDCABA"), 0, 0, {}))
         pass
 
     def test_lcs(self):
@@ -67,6 +172,18 @@ class TestFunctions(unittest.TestCase):
 
         _, lookup = lcs_length(X, Y)
         self.assertEqual([list("MJAU")], lcs(X, Y, len(X), len(Y), lookup))
+
+        self.assertEqual(list("MJAU"), lcs_rec(X, Y, 0, 0, {}))
+
+    def test_lcsubstring_length(self):
+        self.assertEqual(4, lcsubstring_length("ABABC", "BABCA"))
+
+    def test_lcsubstring(self):
+        self.assertEqual("BABC", lcsubstring("ABABC", "BABCA"))
+
+    def test_lps(self):
+        s = "ABBDCACB"
+        self.assertEqual(5, lps_length(s, 0, len(s) - 1, {}))
 
 
 if __name__ == "__main__":
