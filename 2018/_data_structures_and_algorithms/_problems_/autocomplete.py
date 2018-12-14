7652400325888706c8afd60a27ea0c583d711e0c
Daniel Arraes <daniel.arraes@gmail.com>
2018-12-13 19:12:48 -0800

    general push

---
+++ 2018/_data_structures_and_algorithms/_problems_/autocomplete.py
@@ -0,0 +1,87 @@
+class TrieNode:
+    def __init__(self, is_end):
+        self.is_end = is_end
+        self.edges = {}
+
+
+class Trie:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode(is_end=False)
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        i = 0
+        current_node = self.root
+        while i < len(word):
+            if word[i] not in current_node.edges:
+                child = TrieNode(is_end=False)
+                current_node.edges[word[i]] = child
+                current_node = child
+            else:
+                current_node = current_node.edges[word[i]]
+            i += 1
+        current_node.is_end = True
+
+
+    def starts_with(self, prefix):
+        ans = []
+        def _add_all(prefix, node):
+            nonlocal ans
+            if node.is_end:
+                ans.append(prefix)
+
+            for c, next_node in node.edges.items():
+                _add_all(prefix + c, next_node)
+
+        i = 0
+        current_node = self.root
+
+        while i < len(prefix):
+            if prefix[i] not in current_node.edges:
+                return ans
+
+            current_node = current_node.edges[prefix[i]]
+            i += 1
+
+        _add_all(prefix, current_node)
+        return ans
+
+
+####### =============== TESTS ===============
+import unittest
+
+
+class TestFunctions(unittest.TestCase):
+    def test_1(self):
+        trie = Trie()
+        trie.insert("word")
+        trie.insert("facebook")
+        trie.insert("face")
+        trie.insert("facing")
+        trie.insert("book")
+        trie.insert("booking")
+
+        self.assertEqual(["word"], trie.starts_with("word"))
+        self.assertEqual(["word"], trie.starts_with("wor"))
+        self.assertEqual(["book", "booking"], trie.starts_with("boo"))
+        self.assertEqual(["booking"], trie.starts_with("booki"))
+        self.assertEqual(["face", "facebook"], trie.starts_with("face"))
+        self.assertEqual(["face", "facebook", "facing"], trie.starts_with("fac"))
+        self.assertEqual([], trie.starts_with("worf"))
+        #self.assertEqual(trie.starts_with("fac"))
+        #self.assertEqual(trie.starts_with("faci"))
+        #self.assertEqual(trie.starts_with("face"))
+        #self.assertEqual(trie.starts_with("faceb"))
+        #self.assertEqual(trie.starts_with("wor"))
+        #self.assertEqual(trie.starts_with("wording"))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- 2018/_leet_code_/0041_finding_missing_positive.py
+++ 2018/_leet_code_/0041_finding_missing_positive.py
@@ -11,7 +11,7 @@ class Solution:
             return 1
 
         for i in range(n):
-            if nums[i] < 0 or nums[i] >= n:
+            if nums[i] <= 0 or nums[i] >= n:
                 nums[i] = 0
             max_int = max(nums[i], max_int)
 
@@ -32,11 +32,14 @@ import unittest
 class TestFunctions(unittest.TestCase):
     def test_1(self):
         s = Solution()
-        self.assertEqual(1, s.firstMissingPositive([0, 3]))
+        self.assertEqual(4, s.firstMissingPositive([1, 2, 3]))
+        self.assertEqual(4, s.firstMissingPositive([0, 1, 2, 3]))
         self.assertEqual(2, s.firstMissingPositive([1]))
-        self.assertEqual(3, s.firstMissingPositive([1,2,0]))
-        self.assertEqual(2, s.firstMissingPositive([3,4,-1,1]))
-        self.assertEqual(1, s.firstMissingPositive([7,8,9,11,12]))
+        self.assertEqual(1, s.firstMissingPositive([0, 3]))
+        self.assertEqual(3, s.firstMissingPositive([1, 2, 0]))
+        self.assertEqual(2, s.firstMissingPositive([3, 4, -1, 1]))
+        self.assertEqual(1, s.firstMissingPositive([7, 8, 9, 11, 12]))
+
 
 if __name__ == "__main__":
     unittest.main()
