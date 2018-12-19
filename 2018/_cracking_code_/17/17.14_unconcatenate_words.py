732ee2c616ee7d5bd2d6ce522c6c4819e0c4c631
Daniel Arraes <daniel.arraes@gmail.com>
2018-12-19 07:50:40 -0800

    general push

--- 2018/_cracking_code_/17/17.14_unconcatenate_words.py
+++ 2018/_cracking_code_/17/17.14_unconcatenate_words.py
@@ -35,6 +35,38 @@ def unconcatenate_impl(s, words, start, cache):
     return (score, mismatches, res)
 
 
+def unconcatenate_by_misses(s, words):
+    res = unconcatenate_by_misses_impl(s, words, 0, {})
+    print(res)
+    sentence = list(res[1])
+    return " ".join(sentence).strip()
+
+
+def unconcatenate_by_misses_impl(s, words, start, cache):
+    if start >= len(s):
+        return (0, deque())
+
+    if start in cache:
+        return cache[start]
+
+    mismatches, res = 100000, deque()
+
+    for i in range(start + 1, len(s) + 1):
+        if i - start > 10:
+            break
+
+        cur_word = s[start:i]
+        _mismatches, _res = unconcatenate_by_misses_impl(s, words, i, cache)
+        add_mismatches = 0 if cur_word in words else 1
+        if _mismatches + add_mismatches < mismatches:
+            res = copy(_res)
+            res.appendleft(cur_word)
+            mismatches = _mismatches + add_mismatches
+
+    cache[start] = (mismatches, res)
+    return (mismatches, res)
+
+
 ###############################################################
 
 
@@ -46,6 +78,14 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(
             "jess looked just like tim her brother",
             unconcatenate(
+                "jesslookedjustliketimherbrother",
+                ["looked", "just", "like", "her", "brother"],
+            ),
+        )
+
+        self.assertEqual(
+            "jess looked just like tim her brother",
+            unconcatenate_by_misses(
                 "jesslookedjustliketimherbrother",
                 ["looked", "just", "like", "her", "brother"],
             ),
