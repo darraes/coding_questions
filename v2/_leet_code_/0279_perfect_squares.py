725ae5d558381b387e797f8f7c3c1db47644168e
Daniel Arraes <daniel.arraes@gmail.com>
2018-12-29 09:51:41 -0800

    general push

--- v2/_leet_code_/0279_perfect_squares.py
+++ v2/_leet_code_/0279_perfect_squares.py
@@ -1,19 +1,26 @@
 class Solution:
+    def __init__(self):
+        self.lookup = []
+
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
         """
-        lookup = [2 ** 64 - 1] * (n + 1)
-        lookup[0] = 0
-        lookup[1] = 1
+        if n < len(self.lookup):
+            return self.lookup[n]
 
-        for i in range(2, n + 1):
+        start = max(2, len(self.lookup))
+        self.lookup.extend([2 ** 64 - 1] * (n + 1 - len(self.lookup)))
+        self.lookup[0] = 0
+        self.lookup[1] = 1
+
+        for i in range(start, n + 1):
             j = 1
             while j * j <= n:
-                lookup[i] = min(lookup[i], lookup[i - j * j] + 1)
+                self.lookup[i] = min(self.lookup[i], self.lookup[i - j * j] + 1)
                 j += 1
-        return lookup[-1]
+        return self.lookup[-1]
 
 
 ###############################################################
@@ -29,6 +36,9 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(2, s.numSquares(5))
         self.assertEqual(3, s.numSquares(12))
         self.assertEqual(2, s.numSquares(13))
+        self.assertEqual(3, s.numSquares(4635))
+        self.assertEqual(3, s.numSquares(4635))
+        self.assertEqual(2, s.numSquares(1000))
 
 
 if __name__ == "__main__":
