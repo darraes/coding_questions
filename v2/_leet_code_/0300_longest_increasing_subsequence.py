c414c42a3b719f8a2e1f6d691be257a1b2262cfc
Daniel Arraes <daniel.arraes@gmail.com>
2018-12-29 19:12:33 -0800

    general push

--- v2/_leet_code_/0300_longest_increasing_subsequence.py
+++ v2/_leet_code_/0300_longest_increasing_subsequence.py
@@ -4,15 +4,18 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        lookup = [1] * len(nums)
+        if len(nums) == 0:
+            return 0
 
+        lookup = [1] * len(nums)
+        gmax = 1
         for i in range(1, len(nums)):
             for j in range(0, i):
-                bump = 0
                 if nums[j] < nums[i]:
-                    bump = 1
-                lookup[i] = max(lookup[i], bump + nums[j])
-        return lookup[-1]
+                    lookup[i] = max(lookup[i], 1 + lookup[j])
+            gmax = max(gmax, lookup[i])
+
+        return gmax
 
 
 
@@ -23,7 +26,13 @@ import unittest
 class TestFunctions(unittest.TestCase):
     def test_1(self):
         s = Solution()
+        self.assertEqual(3, s.lengthOfLIS([4,10,4,3,8,9]))
         self.assertEqual(4, s.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
+        self.assertEqual(5, s.lengthOfLIS([1, 2, 3, 4, 5]))
+        self.assertEqual(1, s.lengthOfLIS([10, 9, 8, 7, 6]))
+        self.assertEqual(1, s.lengthOfLIS([10, 9]))
+        self.assertEqual(1, s.lengthOfLIS([10]))
+        self.assertEqual(0, s.lengthOfLIS([]))
 
 
 if __name__ == "__main__":
