ebd52091fcdba93b826e6b97acddd02012b5c235
Daniel Arraes <daniel.arraes@gmail.com>
2019-01-11 22:30:33 -0800

    general push

--- v2/_leet_code_/0635_log_storage_skiplist.py
+++ v2/_leet_code_/0635_log_storage_skiplist.py
@@ -2,28 +2,43 @@ from random import random
 
 
 class SkipNode:
-    def __init__(self, key, val, max_level):
+    def __init__(self, key, val, max_levels):
         self.key = key
         self.val = val
-        self.next = [None] * (max_level + 1)
+        self.next = [None] * max_levels
 
 
 class SkipList:
-    def __init__(self, max_level, log_factor):
-        self.max_level = max_level
+    def __init__(self, max_levels, log_factor):
+        self.max_levels = max_levels
         self.log_factor = log_factor
         self.level = 0
-        self.head = SkipNode(0, "", self.max_level)
+        self.head = SkipNode(0, "", self.max_levels)
 
     def random_level(self):
         level = 0
-        while level < self.max_level and random() < self.log_factor:
+        while level < self.max_levels - 1 and random() < self.log_factor:
             level += 1
         return level
 
+    def scan(self, start, end):
+        current = self.head
+        for i in range(self.level, -1, -1):
+            while current.next[i] and current.next[i].key < start:
+                current = current.next[i]
+
+        current = current.next[0]
+
+        ans = []
+        while current and current.key < end:
+            ans.append((current.key, current.val))
+            current = current.next[0]
+        return ans
+
+
     def insert(self, key, val):
         current = self.head
-        b_links = [None] * (self.max_level + 1)
+        b_links = [None] * self.max_levels
 
         for i in range(self.level, -1, -1):
             while current.next[i] and current.next[i].key < key:
@@ -36,20 +51,17 @@ class SkipList:
             current.val = val
             return
 
+        node = SkipNode(key, val, self.max_levels)
         nlevel = self.random_level()
 
-        if nlevel > self.level:
-            for i in range(self.level + 1, nlevel + 1):
-                b_links[i] = self.head
-            self.level = nlevel
-
-        node = SkipNode(key, val, self.max_level)
-        print(key, val)
         for i in range(nlevel + 1):
+            if i > self.level:
+                b_links[i] = self.head
             node.next[i] = b_links[i].next[i]
             b_links[i].next[i] = node
 
-        # Display skip list level wise
+        if nlevel > self.level:
+            self.level = nlevel
 
     def display_list(self):
         head = self.head
@@ -68,18 +80,19 @@ import unittest
 
 class TestFunctions(unittest.TestCase):
     def test_1(self):
-        lst = SkipList(3, 0.5)
-        lst.insert(3, "v123v")
-        lst.insert(6, "v123v")
-        lst.insert(7, "v123v")
-        lst.insert(9, "v123v")
-        lst.insert(12, "v123v")
-        lst.insert(19, "v123v")
-        lst.insert(17, "v123v")
-        lst.insert(26, "v123v")
-        lst.insert(21, "v123v")
-        lst.insert(25, "v123v")
+        lst = SkipList(3, 0.33)
+        lst.insert(3, "1")
+        lst.insert(6, "2")
+        lst.insert(7, "3")
+        lst.insert(9, "4")
+        lst.insert(12, "5")
+        lst.insert(19, "6")
+        lst.insert(17, "7")
+        lst.insert(26, "8")
+        lst.insert(21, "9")
+        lst.insert(25, "10")
         lst.display_list()
+        print(lst.scan(9, 19))
 
 
 if __name__ == "__main__":
