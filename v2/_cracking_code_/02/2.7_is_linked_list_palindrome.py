a21f79f57b7f31d11389b6f8906af51e9765113c
Daniel Arraes <daniel.arraes@gmail.com>
2019-01-27 12:57:59 -0800

    too lazy to write a commit message

--- v2/_cracking_code_/02/2.7_is_linked_list_palindrome.py
+++ v2/_cracking_code_/02/2.7_is_linked_list_palindrome.py
@@ -21,6 +21,31 @@ def is_palindrome(n):
 
     return True
 
+def is_palindrome_2(n):
+    slow = fast = n
+    stack = []
+
+    while fast and fast.next:
+        stack.append(slow)
+        slow = slow.next
+        fast = fast.next.next
+
+    right = None
+    if not fast:
+        right = slow
+    elif not fast.next:
+        right = slow.next
+
+    while stack:
+        if right.val != stack[-1].val:
+            return False
+        stack.pop()
+        right = right.next
+
+    return True
+
+
+
 
 ##################################################
 import unittest
@@ -104,6 +129,76 @@ class TestFunctions(unittest.TestCase):
 
         head = from_list([Node(1), Node(4)])
         self.assertFalse(is_palindrome(head))
+
+    def test_positive2(self):
+        head = from_list(
+            [
+                Node(1),
+                Node(2),
+                Node(3),
+                Node(3),
+                Node(2),
+                Node(1),
+            ]
+        )
+        self.assertTrue(is_palindrome_2(head))
+
+        head = from_list(
+            [Node(1), Node(2), Node(3), Node(2), Node(1)]
+        )
+        self.assertTrue(is_palindrome_2(head))
+
+        head = from_list([Node(1), Node(2), Node(2), Node(1)])
+        self.assertTrue(is_palindrome_2(head))
+
+        head = from_list([Node(1), Node(2), Node(1)])
+        self.assertTrue(is_palindrome_2(head))
+
+        head = from_list([Node(1), Node(1)])
+        self.assertTrue(is_palindrome_2(head))
+
+        head = from_list([Node(1)])
+        self.assertTrue(is_palindrome_2(head))
+
+    def test_negative2(self):
+        head = from_list(
+            [
+                Node(1),
+                Node(2),
+                Node(3),
+                Node(3),
+                Node(4),
+                Node(2),
+                Node(1),
+            ]
+        )
+        self.assertFalse(is_palindrome_2(head))
+
+        head = from_list(
+            [
+                Node(1),
+                Node(2),
+                Node(3),
+                Node(2),
+                Node(4),
+                Node(1),
+            ]
+        )
+        self.assertFalse(is_palindrome_2(head))
+
+        head = from_list(
+            [Node(1), Node(4), Node(2), Node(2), Node(1)]
+        )
+        self.assertFalse(is_palindrome_2(head))
+
+        head = from_list([Node(1), Node(2), Node(1), Node(4)])
+        self.assertFalse(is_palindrome_2(head))
+
+        head = from_list([Node(4), Node(1), Node(1)])
+        self.assertFalse(is_palindrome_2(head))
+
+        head = from_list([Node(1), Node(4)])
+        self.assertFalse(is_palindrome_2(head))
 
 
 if __name__ == "__main__":
