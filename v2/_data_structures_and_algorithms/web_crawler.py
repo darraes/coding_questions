6f12300d07258963ec536530997afb8d9ed2531f
Daniel Arraes <daniel.arraes@gmail.com>
2019-02-02 13:31:05 -0800

    too lazy to write a commit message

--- v2/_data_structures_and_algorithms/web_crawler.py
+++ v2/_data_structures_and_algorithms/web_crawler.py
@@ -3,6 +3,7 @@ import re
 import urllib.error
 import urllib.parse
 from aiohttp import ClientSession
+from threading import Thread, currentThread
 
 HREF_RE = re.compile(r'href="(.*?)"')
 
@@ -35,43 +36,49 @@ class HttpCrawler:
             for i in range(self.w_count):
                 self.workers.append(asyncio.create_task(self.worker(str(i), session)))
 
-            await asyncio.gather(*self.workers, return_exceptions=True)
+            await asyncio.gather(*self.workers)
 
     async def worker(self, wid, session: ClientSession):
+        print("Starting worker ", wid)
         while True:
             url, depth = await self.queue.get()
-            self.crawled.append((wid, depth, url))
+            self.crawled.append((currentThread().ident, wid, depth, url))
 
             if depth < self.max_depth:
-                for next_url in await self.crawl_url(url, session):
+                urls = await self.crawl_url(url, session)
+                for next_url in urls:
                     await self.queue.put((next_url, depth + 1))
 
             self.queue.task_done()
             await asyncio.sleep(0.001)
             if self.queue.empty():
                 break
+        print("Stopping worker ", wid)
 
     async def crawl_url(self, url: str, session: ClientSession, **kwargs):
         next_urls = set()
 
-        html = await self.fetch_html(url, session, **kwargs)
-        for link in HREF_RE.findall(html):
-            if any(x in link for x in self.exclude_extensions):
-                continue
-
-            try:
-                abslink = urllib.parse.urljoin(url, link)
-                abslink, _ = urllib.parse.urldefrag(abslink.strip("/"))
-            except (urllib.error.URLError, ValueError):
-                print("Error parsing URL: %s", link)
-            else:
-                if abslink in self.visited:
+        try:
+            html = await self.fetch_html(url, session, **kwargs)
+            for link in HREF_RE.findall(html):
+                if any(x in link for x in self.exclude_extensions):
                     continue
 
-                self.visited.add(abslink)
-                next_urls.add(abslink)
-                if len(next_urls) == self.max_links_per_page:
-                    break
+                try:
+                    abslink = urllib.parse.urljoin(url, link)
+                    abslink, _ = urllib.parse.urldefrag(abslink.strip("/"))
+                except (urllib.error.URLError, ValueError):
+                    print("Error parsing URL: %s", link)
+                else:
+                    if abslink in self.visited:
+                        continue
+
+                    self.visited.add(abslink)
+                    next_urls.add(abslink)
+                    if len(next_urls) == self.max_links_per_page:
+                        break
+        except Exception as e:
+            print("Error on", url, e)
         return next_urls
 
     async def fetch_html(self, url: str, session: ClientSession, **kwargs) -> str:
@@ -82,12 +89,24 @@ class HttpCrawler:
 
 
 if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    queue = asyncio.Queue()
+    crawler = HttpCrawler(
+        queue, start_urls={"https://www.bloomberg.com/markets/economics"}, workers=10
+    )
+
+    loop.run_until_complete(crawler.craw())
+
+    for c in crawler.crawled:
+        print(c)
+
     queue = asyncio.Queue()
     crawler = HttpCrawler(
         queue, start_urls={"https://www.nytimes.com/guides/"}, workers=10
     )
 
-    loop = asyncio.get_event_loop()
     loop.run_until_complete(crawler.craw())
 
     for c in crawler.crawled:
